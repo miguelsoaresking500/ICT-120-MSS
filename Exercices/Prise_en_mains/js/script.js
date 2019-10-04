51cfd4c3f7684750df7c02721565f8f890fb7cd2
Miguel Soares <miguel.soares@cpnv.ch>
2019-10-04 15:03:37 +0200

    update code

--- Exercices/Prise_en_mains/js/script.js
+++ Exercices/Prise_en_mains/js/script.js
@@ -1,11 +1,17 @@
 let bleu=document.getElementById(  'cssbleutrace');
 let removecss=document.getElementById(  'removecss');
 let changepoli1=document.getElementById( 'changepoli');
+let YES=document.getElementById( 'YES');
+let YESS=document.getElementById( 'YESS');
+let yes1=document.getElementById( 'yes1');
+let yes2=document.getElementById( 'yes2');
 
 bleu.addEventListener( 'click', fnbleu);
 removecss.addEventListener( 'click', fnremovecss);
-changepoli1=document.addEventListener( 'mouseover',fnpolice);
-changepoli1=document.addEventListener( 'mouseout',fnrpolice);
+changepoli1.addEventListener( 'mouseover',fnpolice);
+changepoli1.addEventListener( 'mouseout',fnrpolice);
+
+
 function fnbleu() {
     bleu.classList.add('cssbleutrace')
 }
@@ -21,4 +27,11 @@ function fnrpolice() {
     changepoli1.classList.remove('changepoli')
 }
 
+function fnyes(){
+    if (yes1.checked===true && yes2.checked===true ){
 
+        YES.style.display = "none"
+        YESS.style.display = "block"
+    }
+
+}
