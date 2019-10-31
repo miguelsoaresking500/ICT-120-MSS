b31baeda3c4f02e7dd0fa65422492d39d955f8c2
Miguel Soares <47849626+miguelsoaresking500@users.noreply.github.com>
2019-10-31 21:26:55 +0100

    site voyage

--- PageVoyage/js/script.js
+++ PageVoyage/js/script.js
@@ -1,13 +1,33 @@
 document.addEventListener('DOMContentLoaded', init)
+
 function init() {
-    usr.addEventListener("click", clickinput)
-    pwd.addEventListener("click", clickinput)
+    usr.addEventListener('click', clickinput)
+    pwd.addEventListener('click', clickinput)
+    liste.addEventListener('click', clickliste)
+    ajouter.addEventListener('click', clickajouter)
+    supprimer.addEventListener('click', clicksupprimer)
+    editer.addEventListener('click', clickediter)
 }
-    function clickinput(){
-        document.getElementById('usr').value = "";
-        document.getElementById('pwd').value = "";
-    }
 
+function clickinput() {
+    document.getElementById('usr').value = " ";
+    document.getElementById('pwd').value = " ";
+}
 
+function clickliste() {
+    Tablepersons.style.width = "60";
+    document.getElementById('divchamps').display = "none";
+}
 
+function clickajouter() {
+
+}
+
+function clicksupprimer() {
+
+}
+
+function clickediter() {
+
+}
 
