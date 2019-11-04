87dda1b7c29c643528ad4f64f69f7521e074694f
Miguel Soares <47849626+miguelsoaresking500@users.noreply.github.com>
2019-11-04 23:45:56 +0100

    new website

--- PageVoyage/js/script.js
+++ PageVoyage/js/script.js
@@ -1,25 +1,48 @@
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-
-
-    cmdadd.addEventListener('click', Ajouter)
-
-
+    cmdSave.addEventListener("click",adduser);
+}
+function cache(){
+    table.classList.remove("tab");
 }
 
 
-function Ajouter() {
-
-    console.log()
+function adduser(){
     tr = document.createElement('tr')
-    td1.innerText = txtName.value
+
+    th = document.createElement('th')
     td1 = document.createElement('td')
+    td2 = document.createElement('td')
+    td3 = document.createElement('td')
+    td4 = document.createElement('td')
+    td5 = document.createElement('td')
+    td6 = document.createElement('td')
+    td7 = document.createElement('td')
+    td8 = document.createElement('td')
+    td9 = document.createElement('td')
+
+    th.innerText = txtNum0.value;
+    td1.innerText = txtNum1.value;
+    td2.innerText = txtNum2.value;
+    td3.innerText = txtNum3.value;
+    td4.innerText = txtNum4.value;
+    td5.innerText = txtNum5.value;
+    td6.innerText = txtNum6.value;
+    td7.innerText = txtNum7.value;
+    td8.innerText = txtNum8.value;
+    td9.innerText = txtNum9.value;
+
+    tr.appendChild(th)
     tr.appendChild(td1)
+    tr.appendChild(td2)
+    tr.appendChild(td3)
+    tr.appendChild(td4)
+    tr.appendChild(td5)
+    tr.appendChild(td6)
+    tr.appendChild(td7)
+    tr.appendChild(td8)
+    tr.appendChild(td9)
+
     tblBody.appendChild(tr)
 }
-
-
-
-
-
